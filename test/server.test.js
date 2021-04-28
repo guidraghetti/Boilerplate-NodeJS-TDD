d3c3be8be2b4a2559d49315dd972e6999ce411b0
guidraghetti <guilherme.draghetti@acad.pucrs.br>
2021-04-28 18:15:52 -0300

    adding babel and first test pass

---
+++ test/server.test.js
@@ -0,0 +1,28 @@
+import supertest from "supertest";
+import { config } from "dotenv";
+import app from "../src/app";
+config();
+
+const request = supertest(app);
+
+describe("Test Connections", () => {
+  it("Should connect on PORT 3001", () => {
+    expect(process.env.PORT).toEqual("3001");
+  });
+  it.todo("Should connect to database");
+  it.todo("Should disconnect database when api is off");
+});
+
+describe("User registration", () => {
+  it.todo("Registration fields cannot be empty");
+  it.todo("Password cannot be empty");
+  it.todo("Email cannot be registered more than once ");
+});
+
+describe("User login", () => {
+  it.todo("Email cannot be empty");
+  it.todo("Password cannot be empty");
+  it.todo("User cannot login with random data");
+  it.todo("Login should return a JWT token");
+  it.todo("Should verify if the token is valid");
+});
