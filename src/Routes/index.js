4355d160e834769de0a8ee1d959405fbcaf11883
guidraghetti <guilhermedraghettidev@gmail.com>
2021-05-12 14:53:39 -0300

    Updating CI

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -5,15 +5,15 @@ import userRoute from "./user.route.js";
 import bookRoute from "./book.route.js";
 const rootRouter = Router();
 
-rootRouter.get("/", (req, res) => {
-  res.status(200).json({
-    message: "API is running",
-  });
-});
+// rootRouter.get("/", (req, res) => {
+//   res.status(200).json({
+//     message: "API is running",
+//   });
+// });
 
 export default (app) => {
   app.use(rootRouter);
-  app.use("/swagger", swaggerRouter);
+  app.use("/", swaggerRouter);
   app.use("/auth", authRoute);
   app.use("/user", userRoute);
   app.use("/book", bookRoute);
--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import routes from "./Routes/index.js";
 import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+
 
 routes(app);
 
